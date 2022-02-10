a02daa7c19f8c357781345deccf6d497bb2b07a9
Sakulbuth Ekvittayaniphon <sakulbuth@gmail.com>
2022-02-11 06:11:01 +0700

    back to node

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import sveltePreprocess from 'svelte-preprocess';
-import vercel from '@sveltejs/adapter-vercel';
-// import node from '@sveltejs/adapter-node';
+//import vercel from '@sveltejs/adapter-vercel';
+import node from '@sveltejs/adapter-node';
 // import pkg from './package.json';
 
 
@@ -17,7 +17,8 @@ export default {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: vercel(),
+		adapter: node(),
+		//adapter: vercel(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
